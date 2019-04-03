e3ffd28c4a5927292db8bc4015aafaf4de5948c2
Jack Beauchesne <jackbeauchesne16@gmail.com>
2019-04-03 12:52:43 -0700

    Finished the code

--- planets.py
+++ planets.py
@@ -1,7 +1,24 @@
 def weight_on_planets():
-   # write your code here
+   Weight = input('What do you weigh on earth? ')
    
+   #Using cast to change str -> float
+   EarthWeight = float(Weight)
    
+   #Generate weights on other planets using given values
+   MarsWeight = (EarthWeight * 0.38)
+   JupiterWeight = (EarthWeight * 2.34)
+   
+   #Create blank line before the first sentence
+   #Specify next string is on a different line
+   MarsString = ("\nOn Mars you would weigh %.2f pounds.\n" %MarsWeight)
+   
+   #Format the dloating point to 2 digits after the decimal
+   JupiterString = ("On Jupiter you would weigh %.2f pounds." %JupiterWeight)
+   
+   #Concatenate the 2 strings for the final string
+   FinalString = MarsString + JupiterString
+   
+   print(FinalString, end='')
    
 if __name__ == '__main__':
    weight_on_planets()
